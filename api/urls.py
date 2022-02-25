09a2b434b791e5292175c6b325e222140d904ad5
Micah Stewart <micah@Micahs-MBP.lan>
2022-02-25 13:06:23 -0500

    updated url patterns

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getData),
-    path('add/', views.addComic),
+    path('get-comic/', views.getData),
+    path('add-comic/', views.addComic),
     path('comic-update/<str:pk>/', views.comicUpdate, name="comic-update"),
     path('comic-delete/<str:pk>/', views.comicDelete, name="comic-delete"),
 
