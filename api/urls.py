418ba58e19c0e4d137a3c8d843eaee4effcbeef4
Micah Stewart <micah@Micahs-MBP.lan>
2022-02-25 13:42:56 -0500

    added api_views

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get-comic/', views.getData),
-    path('add-comic/', views.addComic),
+    path('', views.apiOverview, name="api-overview"),
+    path('comic-list/', views.comicList, name="comic-list"),
+    path('comic-detail/<str:pk>/', views.comicDetail, name="comic-detail"),
+    path('comic-create/', views.addComic),
     path('comic-update/<str:pk>/', views.comicUpdate, name="comic-update"),
     path('comic-delete/<str:pk>/', views.comicDelete, name="comic-delete"),
 
