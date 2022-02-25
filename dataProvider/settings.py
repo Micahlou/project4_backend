104493a6a6fd1bf5658ece63360d1ba4dc6e8d83
Micah Stewart <micah@Micahs-MBP.lan>
2022-02-25 12:46:29 -0500

    fixing errors

--- dataProvider/settings.py
+++ dataProvider/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-af58&96jqfakpg6omox&pcr@o_ds@1ug32ii2)$=m8clw0!w1w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://vva-backend.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'vva-backend.herokuapp.com']
 
 
 # Application definition
